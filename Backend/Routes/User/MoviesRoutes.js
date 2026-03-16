2f7b41376a51d3d76d187e1720f23f20e34c5e2e
akash0528 <akashdivakar852@.com>
2026-03-17 01:43:43 +0530

    update backend

--- Backend/Routes/User/MoviesRoutes.js
+++ Backend/Routes/User/MoviesRoutes.js
@@ -3,9 +3,10 @@ import express from "express"
 
 const MoviesRoutes = express.Router();
 
-MoviesRoutes.get("/movies/recent-movies",AdminMovies.recentMovies);
+MoviesRoutes.get("/movies/recent",AdminMovies.recentMovies);
 
-MoviesRoutes.get("/movies/premier-movies",AdminMovies.premier);
+MoviesRoutes.get("/movies/premier",AdminMovies.premier);
 
+MoviesRoutes.get("/movies/:id",AdminMovies.getmovies);
 
 export default MoviesRoutes;
