e138e8e6559e0f4f637418f1394000805f26a6bc
akash0528 <akashdivakar852@.com>
2026-03-16 13:40:01 +0530

    fix homeRoutes

--- Backend/Routes/HomeRoutes.js
+++ Backend/Routes/HomeRoutes.js
@@ -1,6 +1,7 @@
 import Home from "../Controller/Home.js";
 import AdminActivity from "../Controller/AdminActivity.js";
 import AdminArtist from "../Controller/AdminArtist.js"
+import AdminMovies from "../Controller/AdminMovies.js";
 import AdminEvent from "../Controller/AdminEvent.js"
 import express from "express"
 
@@ -13,5 +14,7 @@ HomeRoutes.get("/activity/:id",AdminActivity.GetActivity)
 HomeRoutes.get("/artist/:id",AdminArtist.GetArtist)
 
 HomeRoutes.get("/event/:id",AdminEvent.GetEvents)
+
+HomeRoutes.get("/movies/:id",AdminMovies.getmovies)
 
 export default HomeRoutes;
