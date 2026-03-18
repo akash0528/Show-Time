ba52f9c07a3cf1b2c04177e567448bfdbbf67e5a
akash0528 <akashdivakar852@.com>
2026-03-18 17:37:02 +0530

    Update Backend

--- Backend/Services/otp.js
+++ Backend/Services/otp.js
@@ -4,7 +4,7 @@ const sendOtp = async (email, otp) => {
     try {
         const transporter = nodemailer.createTransport({
            host: "smtp-relay.brevo.com",
-           port: 587, 
+           port: 2525, 
             secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
